b36b7f3c4a167d9b6bf7ce373bfa7158acfad1e0
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2021-04-23 12:24:17 -0300

    formulario de contacto terminado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
-import Metadata from "../components/Metadata"
-import Menu from "../components/Menu.jsx"
-import "../styles/reset.min.css"
-import "../styles/global.css"
-import "../styles/index.css"
+import Head from "../components/Head"
+import Nav from "../components/Nav.js"
+import { css } from "@emotion/react"
+import "./reset.min.css"
+import "./index.css"
 
-export default function IndexPage() {
+const IndexPage = () => {
   return (
     <>
-      <Metadata />
-      <Menu />
-      <main className="main">
-        <section className="section">
-          <h1 className="section__title">
-            Hello, <br />
-            we’re WiDev, <br />
-            a web developer <br />
-            team
-          </h1>
-          <Link className="button" to="/contact">
-            Contactanos
-          </Link>
-          
-        </section>
-      </main>
+      <Head />
+
+      <Nav />
+
+      <h1 className="title-h1">
+        Hello,
+        <br />
+        we’re WiDev, a web developer team
+      </h1>
+
+      <Link
+        to="/contact"
+        css={css`
+          text-decoration: none;
+        `}
+      >
+        <button className="contacto">Contactanos</button>
+      </Link>
     </>
   )
 }
+
+export default IndexPage
